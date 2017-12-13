e8efb4baf309c8d9b5656b5b44b01edf31495ae0
Nikesh Nazareth <nikesh.nazareth.developer@gmail.com>
2017-12-13 15:46:47 +1100

    Updated gitignore

--- initialise.sh
+++ initialise.sh
@@ -2,13 +2,9 @@
 
 read -p "Enter app prefix: " prefix
 
-echo -e "\nCreating .gitignore..."
-echo -e "# Webstorm IDE\n.idea\n" > .gitignore
-echo -e "# Dependencies\nnode_modules\n" >> .gitignore
-
 echo "Installing Angular CLI..."
 npm install -g @angular/cli
 
 echo "Creating new angular app..."
-ng new app --prefix $prefix
+ng new $prefix-app --prefix $prefix
 
